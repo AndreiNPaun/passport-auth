c38bbc15882c908c2db7a0e3841684311a2cc3ad
andrei <andreinicolaepaun@gmail.com>
2023-08-08 12:42:16 +0100

    small updates

---
+++ client/src/pages/CreateAccount.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import CreateAccountForm from '../components/Authentication/CreateAccountForm';
+
+const CreateAccount = () => {
+  return <CreateAccountForm />;
+};
+
+export default CreateAccount;
--- client/src/pages/UserForm.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Form from '../components/Authentication/Form';
-
-const UserForm = () => {
-  return <Form />;
-};
-
-export default UserForm;
