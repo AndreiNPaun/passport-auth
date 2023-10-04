48fd3f9898e0a9bd541ff8b3000291d461a7365d
Andrei <andreinicolaepaun@google.com>
2023-10-04 16:43:39 +0300

    token validation partially integrated in react router dom

--- client/src/components/AccountManagement/AccountManagement.js
+++ client/src/components/AccountManagement/AccountManagement.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from '@chakra-ui/react';
 
 import EditProfile from './EditProfile';
