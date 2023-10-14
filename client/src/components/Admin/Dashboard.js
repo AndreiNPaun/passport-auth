b8b244cb24ef52e316dcffdb146f52390c1f5673
Andrei <andreinicolaepaun@google.com>
2023-10-14 20:00:36 +0100

    list users controller added

--- client/src/components/Admin/Dashboard.js
+++ client/src/components/Admin/Dashboard.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import httpRequest from '../../utils/httpRequest';
 
 const Dashboard = () => {
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await httpRequest(
+        'get',
+        `${process.env.REACT_APP_SERVER_URL}/list-users`
+      );
+      console.log('response', response);
+
+      setUserList(response);
+    })();
+  }, []);
+
   return <>Yo</>;
 };
 
