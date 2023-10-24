9e7407cc3f582faba27308144f7fd2b02b4733e6
Andrei <andreinicolaepaun@google.com>
2023-10-24 18:27:13 +0100

    authenticate and decodeToken unit tests

--- server/middleware/authorize.test.js
+++ server/middleware/authorize.test.js
@@ -1,48 +1,46 @@
-import { describe, it, expect, vi, beforeEach } from 'vitest';
+import { it, expect, vi, beforeEach } from 'vitest';
 
 import authorize from './authorize';
 
-describe('Authorize Middleware', () => {
-  let req;
-  let res;
-  let next;
+let req;
+let res;
+let next;
 
-  beforeEach(() => {
-    req = {};
-    res = {
-      status: vi.fn(() => res),
-      send: vi.fn(),
-    };
-    next = vi.fn();
-  });
+beforeEach(() => {
+  req = {};
+  res = {
+    status: vi.fn(() => res),
+    send: vi.fn(),
+  };
+  next = vi.fn();
+});
 
-  it('should only call next if role is admin', () => {
-    req.role = 'admin';
+it('should only call next if role is admin', () => {
+  req.role = 'admin';
 
-    authorize(req, res, next);
+  authorize(req, res, next);
 
-    expect(res.status).not.toHaveBeenCalled();
-    expect(res.send).not.toHaveBeenCalled();
-    expect(next).toHaveBeenCalled();
-  });
+  expect(res.status).not.toBeCalled();
+  expect(res.send).not.toBeCalled();
+  expect(next).toBeCalled();
+});
 
-  it('should only call next if role is moderator', () => {
-    req.role = 'admin';
+it('should only call next if role is moderator', () => {
+  req.role = 'admin';
 
-    authorize(req, res, next);
+  authorize(req, res, next);
 
-    expect(res.status).not.toHaveBeenCalled();
-    expect(res.send).not.toHaveBeenCalled();
-    expect(next).toHaveBeenCalled();
-  });
+  expect(res.status).not.toBeCalled();
+  expect(res.send).not.toBeCalled();
+  expect(next).toBeCalled();
+});
 
-  it('should send status code 401 if role is not admin or moderator', () => {
-    req.role = 'guest';
+it('should send status code 401 if role is not admin or moderator', () => {
+  req.role = 'guest';
 
-    authorize(req, res, next);
+  authorize(req, res, next);
 
-    expect(res.status).toHaveBeenCalledWith(401);
-    expect(res.send).toHaveBeenCalled();
-    expect(next).not.toHaveBeenCalled();
-  });
+  expect(res.status).toBeCalledWith(401);
+  expect(res.send).toBeCalled();
+  expect(next).not.toBeCalled();
 });
