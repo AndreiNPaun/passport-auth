83a6448e9e8b17185365f8aa51de66edc5a36bce
Andrei <andreinicolaepaun@google.com>
2023-11-30 17:12:43 +0000

    fix

---
+++ client/src/utils/HttpRequest.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+// Custom hook for axios requests
+const HttpRequest = async (method, url, values = {}) => {
+  const options = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+  };
+
+  if (['post', 'put', 'patch'].includes(method)) {
+    options.data = values;
+  } else if (method === 'get') {
+    options.params = values;
+  } else {
+    throw new Error('Invalid HTTP Request Method.');
+  }
+
+  const response = await axios({ method, url, ...options });
+  return response;
+};
+
+export default HttpRequest;
